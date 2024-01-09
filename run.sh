555b1690fb45ec2fcff383bd8cdbee77dce6d31e
lmh <dev@hilberg.eu>
2024-01-09 17:31:08 +0100

    adjust local docker run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE="ghcr.io/tna76874/docdepot:latest"
-
+IMAGE="ghcr.io/tna76874/docdepot:edge"
+docker build -t ${IMAGE} .
 docker run -p 5000:5000 --name docdeposer-container --rm -v $(pwd)/data:/app/data ${IMAGE}
